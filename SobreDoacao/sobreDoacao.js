0af72addc935d59e2a7af08446dff025c88a3728
amandafariassena <amandafariasssena@gmail.com>
2019-11-27 15:37:25 -0300

    popup doação realizada com sucesso

---
+++ SobreDoacao/sobreDoacao.js
@@ -0,0 +1,7 @@
+var popUpAparece = document.getElementById("popUpAparecer");
+var modalBg = document.querySelector(".popup");
+
+popUpAparece.addEventListener("click", function() {
+    modalBg.classList.add("popup-active");
+});
+
